09b4380e7e84c4e84064a4f117da4e38099abad1
unknown <faisaljfcl@gmail.com>
2022-08-18 10:43:38 +0600

    Added Clear, SQRT and SQR buttons

--- index.js
+++ index.js
@@ -30,16 +30,23 @@ function calculator(v1, v2, operator) {
       return 0;
   }
 }
+//--------------------                  --------------------//
 
-//Targets for EventListeners
+//-------------------- Targets for EventListeners --------------------//
+
+//--------- GROUP of BUTTONS --------------//
 const NumericalPad = document.getElementById("numbers");
 const OperatorButtons = document.getElementById("operators");
+
+//--------- INDIVIDUAL BUTTONS --------------//
 const EqualButton = document.getElementById("equal-operator");
+const ClearButton = document.getElementById("clear-button");
 //--------------------                  --------------------//
 
 //Setting Display as Global Variable
 var Display = document.getElementById("display");
 
+//-------------------- EventListeners --------------------//
 NumericalPad.addEventListener("click", function (e) {
   //Preventing Cascade Button Clicking
   //if outside the button clicked
@@ -83,3 +90,19 @@ EqualButton.addEventListener("click", function (e) {
   setElementValue("operator-sign", "");
   e.stopPropagation();
 });
+//--------------------                  --------------------//
+
+//-------------------- Event Functions --------------------//
+function clearDisplay() {
+  setElementValue("display", "0");
+}
+
+function SQR() {
+  const currentlyDisplaying = getElementValue("display");
+  setElementValue("display", currentlyDisplaying * currentlyDisplaying);
+}
+
+function SQRT() {
+  const currentlyDisplaying = getElementValue("display");
+  setElementValue("display", Math.sqrt(currentlyDisplaying));
+}
